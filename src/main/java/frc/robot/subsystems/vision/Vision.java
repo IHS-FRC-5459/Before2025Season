18cd1f0609fd131f203856ee9091ca7af90e5e48
Cole <coltonflather@gmail.com>
2025-10-30 19:54:25 -0400

    Fusion v1

--- src/main/java/frc/robot/subsystems/vision/Vision.java
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -51,10 +51,71 @@ public class Vision extends SubsystemBase {
   }
 
   private Pose2d fusedPose = new Pose2d();
-  private Matrix<N3, N1> fusedStdDevs = VecBuilder.fill(0.7, 0.5, 0.2);
-
-  // To be implimented. Will update fusedPose and fusedStdDevs
-  public void fuse(Pose2d[] poses, Matrix<N3, N1>[] stdDevsArray) {}
+  private Matrix<N3, N1> fusedStdDevs = VecBuilder.fill(99999, 99999, 99999);
+  private boolean fusedPoseUpdated = false;
+  // Check time units
+  private double fusedTime = Timer.getFPGATimestamp();
+  // Updates fusedPose and fusedStdDevs
+  public boolean fuse(Pose2d[] poses, Matrix<N3, N1>[] stdDevs) {
+    fusedPoseUpdated = true;
+    double possibleTime = Timer.getFPGATimestamp();
+    // Shoudl alwyas be flase, but just in case
+    if (poses.length != stdDevs.length) {
+      Logger.recordOutput("PhotonvisionLogging/Hit length mismatch in fuse", true);
+      fusedPoseUpdated = false;
+      return false;
+    }
+    // First check if each pose is in the field
+    ArrayList<Pose2d> inFieldPoses = new ArrayList<Pose2d>();
+    ArrayList<Matrix> inFieldStdDevs = new ArrayList<Matrix>();
+    for (int i = 0; i < poses.length; i++) {
+      Pose2d pose = poses[i];
+      // X check
+      double xDeadspace = 0.05; // For each direction
+      if (pose.getX() < 0 - xDeadspace || pose.getX() > 17.5387 + xDeadspace) {
+        continue;
+      }
+      // Y check
+      double yDeadspace = 0.05; // For each direction
+      if (pose.getY() < 0 - yDeadspace || pose.getY() > 8.0518 + yDeadspace) {
+        continue;
+      }
+      // Now in field, add it to list
+      inFieldPoses.add(pose);
+      inFieldStdDevs.add(stdDevs[i]);
+    }
+    if (inFieldPoses.size() == 0) {
+      Logger.recordOutput("Poses in field length is 0", true);
+      fusedPoseUpdated = false;
+      return false;
+    }
+    if (inFieldPoses.size() == 1) {
+      fusedPose = inFieldPoses.get(0);
+      fusedStdDevs = inFieldStdDevs.get(0);
+      fusedPoseUpdated = true;
+      fusedTime = possibleTime;
+      return true;
+    }
+    // Use best pose
+    double bestAverage = 999999999;
+    Matrix<N3, N1> bestStdDevs = inFieldStdDevs.get(0);
+    Pose2d bestPose = inFieldPoses.get(0);
+    for (int i = 1; i < inFieldStdDevs.size(); i++) {
+      double average =
+          inFieldStdDevs.get(i).elementSum()
+              / (inFieldStdDevs.get(i).getNumRows() * inFieldStdDevs.get(i).getNumCols());
+      if (average < bestAverage) {
+        bestAverage = average;
+        bestStdDevs = inFieldStdDevs.get(i);
+        bestPose = inFieldPoses.get(i);
+      }
+    }
+    fusedPoseUpdated = true;
+    fusedPose = bestPose;
+    fusedStdDevs = bestStdDevs;
+    fusedTime = possibleTime;
+    return true;
+  }
 
   public Pose2d getFusedPose() {
     return fusedPose;
@@ -62,6 +123,10 @@ public class Vision extends SubsystemBase {
 
   public Matrix<N3, N1> getFusedStdDevs() {
     return fusedStdDevs;
+  }
+
+  public double getFusedTime() {
+    return fusedTime;
   }
 
   @Override
@@ -84,12 +149,13 @@ public class Vision extends SubsystemBase {
     swerveEstimator.update(pigeon.getRotation2d(), drive.getModulePositions());
     Logger.recordOutput("photonvisionLogging/pigeonRot", pigeon.getRotation2d().getDegrees());
 
-    if (posesArr.length > 0 && stdDevsArr.length > 0) {
-      fuse(posesArr, stdDevsArr);
+    if (fuse(posesArr, stdDevsArr)) {
       swerveEstimator.addVisionMeasurement(
-          this.getFusedPose(), Timer.getFPGATimestamp(), this.getFusedStdDevs().times(2));
+          this.getFusedPose(), this.getFusedTime(), this.getFusedStdDevs().times(2));
       Logger.recordOutput("photonvisionLogging/isUpdatingWCameras", true);
     } else {
+      swerveEstimator.addVisionMeasurement(
+          this.getFusedPose(), this.getFusedTime(), this.getFusedStdDevs().times(99));
       Logger.recordOutput("photonvisionLogging/isUpdatingWCameras", false);
     }
     // This method will be called once per scheduler run
